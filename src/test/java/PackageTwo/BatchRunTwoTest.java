62c30bb626d38bfe95687812e8fce3720a838f9a
ganeshprasadd <ganesh.prasadd1199@gmail.com>
2023-12-16 00:28:09 +0530

    comit

---
+++ src/test/java/PackageTwo/BatchRunTwoTest.java
@@ -0,0 +1,18 @@
+package PackageTwo;
+
+import org.testng.annotations.Test;
+
+public class BatchRunTwoTest {
+	
+	@Test
+	public void pushBatchRunTwoTest() {
+		
+		System.out.println("Method belong to Project 2 - firtst method Batch Second Run");
+	}
+	
+	public void pushBatchRunTwoMethodTest() {
+		
+		System.out.println("Method belong to Project 2 - second method Batch Second Run");
+	}
+
+}
