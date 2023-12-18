c70d557ccde4a52ffda1bdb6ccad00b46e201a58
ganeshprasadd <ganesh.prasadd1199@gmail.com>
2023-12-18 12:46:02 +0530

    pushing parameter test case to git

---
+++ src/test/java/PackageTwo/ParametersTest.java
@@ -0,0 +1,20 @@
+package PackageTwo;
+
+import org.testng.annotations.Test;
+
+public class ParametersTest {
+	
+	@Test
+	public void m1() {
+		
+		String BROWSER = System.getProperty("browser");
+		String URL = System.getProperty("url");
+		
+		System.out.println(BROWSER);
+		System.out.println(URL);
+		
+		
+		
+	}
+
+}
